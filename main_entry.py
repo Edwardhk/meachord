671232c9a647afca9d3d80c7e44521876d5d07b1
Edward <hkm727145@gmail.com>
2020-11-13 02:46:02 +0800

    [INIT] Restructured with plot and log msgs

--- main_entry.py
+++ main_entry.py
@@ -6,7 +6,7 @@ import librosa.display
 from config import Config
 import sounddevice as sd
 
-DURATION = 20
+DURATION = 10
 N_CHROMA = 12
 HOP_LENGTH = 512
 CHORDS_ARR = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
@@ -14,31 +14,44 @@ CHORDS_ARR = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 def load_audio(filename, dur=None):
     print("[LOAD] Start loading audio...")
-    load_y, load_sr = librosa.load(filename, duration=dur)
-
-    minute = int(load_y.shape[0] / load_sr / 60)
-    second = load_y.shape[0] / load_sr % 60
+    y, sr = librosa.load(filename, duration=dur)
+    minute = int(y.shape[0] / sr / 60)
+    second = y.shape[0] / sr % 60
     print(f"[LOAD] Loaded audio with {minute} minutes and {second:.2f} seconds...")
-    return load_y, load_sr
+    print(f"[LOAD] Sample rate: {sr}")
+    return y, sr
 
 
 def play_audio(y, sr):
     sd.play(y, sr, blocking=True)
 
 
-if __name__ == '__main__':
-    y, sr = load_audio('src/audio_input/Apologize.flac', dur=30)
-    chroma_cq = librosa.feature.chroma_cqt(y=y, sr=sr,
-                                           hop_length=HOP_LENGTH, n_chroma=N_CHROMA)
-    max_pitch_index = np.argmax(chroma_cq, axis=0)
+def get_chroma_cqt(y, sr, verbose=False):
+    chroma_cqt = librosa.feature.chroma_cqt(y=y, sr=sr,
+                                            hop_length=HOP_LENGTH, n_chroma=N_CHROMA)
+    max_pitch_index = np.argmax(chroma_cqt, axis=0)
+    if verbose:
+        librosa.display.specshow(chroma_cqt, y_axis='chroma', x_axis='time')
+        plt.show()
+        print(f"[CQT] Shape of CQT array: {chroma_cqt.shape}")
+        print(f"[CQT] Shape of max_pitch array: {max_pitch_index.shape}")
+    return chroma_cqt, max_pitch_index
+
+
+def main():
+    y, sr = load_audio('src/audio_input/Apologize.flac', dur=DURATION)
+    chroma_cqt, max_pitch_index = get_chroma_cqt(y, sr, verbose=True)
 
     pitch_count_dict = {}
-
-    for i in range(int(sr/HOP_LENGTH)):
+    for i in range(max_pitch_index.shape[0]):
         max_pitch = CHORDS_ARR[max_pitch_index[i]]
         if max_pitch not in pitch_count_dict:
             pitch_count_dict[max_pitch] = 0
         else:
             pitch_count_dict[max_pitch] += 1
     print(pitch_count_dict)
+
+
+if __name__ == '__main__':
+    main()
 
