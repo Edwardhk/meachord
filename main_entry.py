337794ef1cdb2da01e4f72071041a5a7de9ad7d6
Edward <hkm727145@gmail.com>
2020-11-14 02:55:59 +0800

    [MAIN] Connected with chord utils and test

--- main_entry.py
+++ main_entry.py
@@ -3,22 +3,22 @@ import numpy as np
 import librosa
 import soundfile as sf
 import librosa.display
-from config import Config
+from chords_util import Chords_Util
 import sounddevice as sd
 
-DURATION = 10
+SAMPLE_RATE = 44100
+DURATION = 20
 N_CHROMA = 12
 HOP_LENGTH = 512
-CHORDS_ARR = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-
 
 def load_audio(filename, dur=None):
     print("[LOAD] Start loading audio...")
-    y, sr = librosa.load(filename, duration=dur)
+    y, sr = librosa.load(filename, duration=dur, sr=SAMPLE_RATE)
     minute = int(y.shape[0] / sr / 60)
     second = y.shape[0] / sr % 60
     print(f"[LOAD] Loaded audio with {minute} minutes and {second:.2f} seconds...")
     print(f"[LOAD] Sample rate: {sr}")
+    print(f"[LOAD] Data shape: {y.shape}")
     return y, sr
 
 
@@ -38,18 +38,36 @@ def get_chroma_cqt(y, sr, verbose=False):
     return chroma_cqt, max_pitch_index
 
 
-def main():
-    y, sr = load_audio('src/audio_input/Apologize.flac', dur=DURATION)
-    chroma_cqt, max_pitch_index = get_chroma_cqt(y, sr, verbose=True)
+def get_onset_data(y, sr):
+    onset_env = librosa.onset.onset_strength(y, sr=sr)
+    # onset_frames = librosa.onset.onset_detect(y, sr=sr, wait=1,
+    #                                           pre_avg=1, post_avg=1, pre_max=1, post_max=1,
+    #                                           backtrack=True)
+    onset_frames = librosa.util.peak_pick(onset_env, 7, 7, 7, 7, 0.5, 5)
+    onset_times = librosa.frames_to_time(onset_frames)
+    clicks = librosa.clicks(frames=onset_frames, sr=sr, length=len(y))
+    # play_audio(y+clicks, sr)
+    #dtempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr,
+                                # aggregate=None)
+    #print(dtempo/60)
 
-    pitch_count_dict = {}
-    for i in range(max_pitch_index.shape[0]):
-        max_pitch = CHORDS_ARR[max_pitch_index[i]]
-        if max_pitch not in pitch_count_dict:
-            pitch_count_dict[max_pitch] = 0
-        else:
-            pitch_count_dict[max_pitch] += 1
-    print(pitch_count_dict)
+
+def main():
+    chords_util = Chords_Util()
+    vecs = chords_util.generate_vectors()
+    y, sr = load_audio('src/audio_input/Janice.wav', dur=DURATION)
+    chroma_cqt, max_pitch_index = get_chroma_cqt(y, sr)
+    # get_onset_data(y, sr)
+    # print(dtempo)
+
+    # pitch_count_dict = {}
+    # for i in range(max_pitch_index.shape[0]):
+    #     max_pitch = BASE_NOTE[max_pitch_index[i]]
+    #     if max_pitch not in pitch_count_dict:
+    #         pitch_count_dict[max_pitch] = 0
+    #     else:
+    #         pitch_count_dict[max_pitch] += 1
+    # print(pitch_count_dict)
 
 
 if __name__ == '__main__':
