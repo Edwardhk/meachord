35fda1cefdbb90b587962fdd485863f8083c0433
Edward <hkm727145@gmail.com>
2020-11-26 01:43:24 +0800

    [CONFIG] Reduced threshold due to duplicated chord detection

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config:
     def __init__(self):
         self.sr = 44100
-        self.dur = 10
+        self.dur = 60
         self.n_chroma = 12
         self.hop_length = 512
-        self.threshold = 100
-        self.verbose = True
+        self.threshold = 40
+        self.verbose = False
